9a55646ea8c765520735486d90dd9f70edad9b0b
Victor-PV <victorperezvillanueva123@gmail.com>
2024-03-03 17:57:29 +0100

    favicon added

--- pokedex-pokeapi/src/app/pages/pokedex/pokedex-body/pokedex-body.component.ts
+++ pokedex-pokeapi/src/app/pages/pokedex/pokedex-body/pokedex-body.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit,  Directive, Input, QueryList, ViewChildren } from '@angular/core';
 import { REGIONES } from 'src/app/constants/global.constants';
 import { Region } from '../../../interfaces/region';
 import { ApiService } from 'src/app/services/api.service';
+import { debug } from 'console';
+
+@Directive({selector: 'pane'})
+export class Pane {
+  @Input() val!: string;
+}
 
 @Component({
   selector: 'app-pokedex-body',
   templateUrl: './pokedex-body.component.html',
   styleUrls: ['./pokedex-body.component.scss']
 })
-export class PokedexBodyComponent implements OnInit {
+export class PokedexBodyComponent implements OnInit, AfterViewInit {
 
   data : any = {};
 
   regiones : any = REGIONES;
-  regionActual : Region = this.regiones.KANTO;
+  regionActual : Region = this.regiones.SINNOH;
 
 
   constructor(private apiService : ApiService) { }
 
   ngOnInit(): void {
+    console.log(this.checkRegionLocalStorage(this.regionActual) ? "LOAD FROM LOCAL STORAGE" : "LOAD FROM API")
     if(this.checkRegionLocalStorage(this.regionActual))
       this.fillListFromLocalStorage(this.regionActual);
     else
       this.fillListFromApi(this.regionActual);
+      //console.log(this.data)
 
-      console.log(this.data)
+  }
 
+  @ViewChildren("pokemonNameTag") divs: QueryList<any> | undefined
+  
+  ngAfterViewInit() {
+    this.divs?.forEach(div => {
+      this.printOneCardName(div.nativeElement.id)
+    });    
   }
 
   fillListFromApi(region : Region){
@@ -34,17 +48,11 @@ export class PokedexBodyComponent implements OnInit {
     this.apiService.getAllPokemonByRegion(region).subscribe(data => {
       this.data = data.results;
       data.results.forEach((item: any) => {
-        //Carga la etiqueta del nombre del pokemon, si la pilla ejecuta el codigo
-        let element : any = document.getElementById("pokemonNameTag"+(region.desde+pokemonIndexCounter));
-        if(element)
-          element.innerHTML = item.name.charAt(0).toUpperCase() + item.name.substring(1); //Sustituye la primera letra por mayuscula
-
         // Guarda los pokemon en una lista que posteriormente guardaremos en el localstorage para evitar llamadas
         listaProvisional.push({
           id: region.desde+pokemonIndexCounter,
           name: item.name
         });
-
         pokemonIndexCounter++;
       });
 
@@ -53,16 +61,46 @@ export class PokedexBodyComponent implements OnInit {
       let a : any = window.localStorage.getItem("region_"+region.nombre);
       //console.log(JSON.parse(a));
       //console.log(this.data)
-      console.log(this.data)
+      this.printCardsName(listaProvisional, region)
     })
   }
 
   fillListFromLocalStorage(region : Region){
     let list : any = window.localStorage.getItem("region_"+region.nombre);
-    if(list)
+    if(list){
       this.data = JSON.parse(list);
-    else
+      this.printCardsName(this.data, region)
+    }else{
       this.fillListFromApi(region)
+    }
+
+  }
+
+  // Se ejecuta cuando lo llama la propia carta desde el HTML cuando carga al iniciar la pagina
+  printOneCardName(id: string){
+    let element : any = document.getElementById(id);
+    if(element){
+      let list : any = window.localStorage.getItem("region_"+this.regionActual.nombre);
+      list = JSON.parse(list)
+      if(list){
+        list.forEach((item: any) => {
+          if("pokemonNameTag"+item.id == id) 
+            element.innerHTML = item.name.charAt(0).toUpperCase() + item.name.substring(1); 
+        })
+      }
+    }
+      
+  }
+
+  printCardsName(listaProvisional : [], region: Region){
+    let pokemonIndexCounter : number = 0;
+    listaProvisional.forEach((item: any) => {
+      //Carga la etiqueta del nombre del pokemon, si la pilla ejecuta el codigo
+      let element : any = document.getElementById("pokemonNameTag"+(region.desde+pokemonIndexCounter++));
+      //console.log(element)
+      if(element)
+        element.innerHTML = item.name.charAt(0).toUpperCase() + item.name.substring(1); //Sustituye la primera letra por mayuscula
+    })
   }
 
   printPokemon(){
